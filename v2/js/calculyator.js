155b964a842788340b13267f9ad82b5e67d7c7ab
михаил воевудский <michael.voevudskiy@gmail.com>
2022-10-03 22:28:39 +0300

    first commit

---
+++ v2/js/calculyator.js
@@ -0,0 +1,58 @@
+
+let grn_min = 65; //грн-минута
+
+function myCalculyator(){
+
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', './basa.json');
+    xhr.responseType = 'json';
+    xhr.send();
+    
+    // тело ответа {"сообщение": "Привет, мир!"}
+    xhr.onload = function() {
+     let mater = xhr.response;
+      // console.log(mater); // Привет, мир!
+      // mater = JSON.parse(responseObj);
+       clac(mater);
+    };
+};
+
+function clac(mater){
+
+    console.log(mater)
+    res_sp.innerHTML = "думаю...";
+    
+    let mat = document.getElementById('materials_choice').value;
+    let tin = document.getElementById('thickness_choice').value;
+    let cond = document.getElementById('quality_choice').value;
+    let mery = document.getElementById('length_choice').value;    
+
+    let result = (1000/ mater[mat][cond][tin]) * grn_min * mery;
+    result = Math.floor(result * 10)/10;
+    setTimeout(function() {
+        if (result<600){
+            res_sp.innerHTML = 'Фактическая стоимость: ' + result + ' грн.';
+            calcMess.innerHTML = 'Минимальная стоимость заказа - 1000грн'
+        }
+        else{
+             res_sp.innerHTML = 'Стоимость заказа: ' + result + ' грн';
+             calcMess.innerHTML = null;
+        }
+    }, 700);
+}
+
+function imgchange(value){
+    if(value=="rz60"){
+        imgq.src = "./jpg/2.jpg";
+        qtext.innerHTML = "Стандартное качество - подходит для 90% задачь, точность до 0,4-0,2мм"
+    }
+    else if (value == "rz80"){
+        imgq.src = "./jpg/4.jpg";
+        qtext.innerHTML = "Грубое качество - для сварных, неточных деталей, точность - 0,5-0,7мм"
+    }
+
+    else{
+        imgq.src = "./jpg/1.jpg";
+        qtext.innerHTML = "Высокое качестко - для точных изделий, точность до 0,1мм"
+    }
+}
